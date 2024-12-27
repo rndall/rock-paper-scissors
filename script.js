41e25231e692da48d66d9cad8d7f0eeff5ab4b5e
Ran <96964503+rndall@users.noreply.github.com>
2024-12-27 14:55:28 +0800

    Remove playGame() function

--- script.js
+++ script.js
@@ -15,54 +15,48 @@ function getHumanChoice() {
 	return prompt("'rock', 'paper', 'scissors'?");
 }
 
-function playGame() {
-	let humanScore = 0;
-	let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-	function playRound(humanChoice, computerChoice) {
-		humanChoiceLower = humanChoice.toLowerCase();
-		humanChoiceTitle = humanChoiceLower.replace(
-			humanChoiceLower.at(0),
-			humanChoice.toUpperCase().at(0),
-		);
-		computerChoiceTitle = computerChoice.replace(
-			computerChoice.at(0),
-			computerChoice.toUpperCase().at(0),
-		);
+function playRound(humanChoice, computerChoice) {
+	humanChoiceLower = humanChoice.toLowerCase();
+	humanChoiceTitle = humanChoiceLower.replace(
+		humanChoiceLower.at(0),
+		humanChoice.toUpperCase().at(0),
+	);
+	computerChoiceTitle = computerChoice.replace(
+		computerChoice.at(0),
+		computerChoice.toUpperCase().at(0),
+	);
 
-		if (
-			(humanChoiceLower === "rock" && computerChoice === "scissors") ||
-			(humanChoiceLower === "paper" && computerChoice === "rock") ||
-			(humanChoiceLower === "scissors" && computerChoice === "paper")
-		) {
-			humanScore++;
-			console.log(`You win! ${humanChoiceTitle} beats ${computerChoiceTitle}`);
-		} else if (
-			(computerChoice === "rock" && humanChoiceLower === "scissors") ||
-			(computerChoice === "paper" && humanChoiceLower === "rock") ||
-			(computerChoice === "scissors" && humanChoiceLower === "paper")
-		) {
-			computerScore++;
-			console.log(`You lose! ${computerChoiceTitle} beats ${humanChoiceTitle}`);
-		} else {
-			console.log("It's a tie!");
-		}
-	}
-
-	for (let i = 0; i < 5; i++) {
-		const humanSelection = getHumanChoice();
-		const computerSelection = getComputerChoice();
-
-		playRound(humanSelection, computerSelection);
-	}
-
-	if (humanScore > computerScore) {
-		console.log("You won the game!");
-	} else if (humanScore < computerScore) {
-		console.log("You lost the game.");
+	if (
+		(humanChoiceLower === "rock" && computerChoice === "scissors") ||
+		(humanChoiceLower === "paper" && computerChoice === "rock") ||
+		(humanChoiceLower === "scissors" && computerChoice === "paper")
+	) {
+		humanScore++;
+		console.log(`You win! ${humanChoiceTitle} beats ${computerChoiceTitle}`);
+	} else if (
+		(computerChoice === "rock" && humanChoiceLower === "scissors") ||
+		(computerChoice === "paper" && humanChoiceLower === "rock") ||
+		(computerChoice === "scissors" && humanChoiceLower === "paper")
+	) {
+		computerScore++;
+		console.log(`You lose! ${computerChoiceTitle} beats ${humanChoiceTitle}`);
 	} else {
-		console.log("The game was tied.");
+		console.log("It's a tie!");
 	}
 }
 
-playGame();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+if (humanScore > computerScore) {
+	console.log("You won the game!");
+} else if (humanScore < computerScore) {
+	console.log("You lost the game.");
+} else {
+	console.log("The game was tied.");
+}
